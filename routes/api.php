468b8a8e07ae01adf33afbad8dec3228f469280b
Febryan Pratama <febryancaesarpratama@gmail.com>
2024-09-05 10:26:20 +0700

    feat: add callback controller

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Api\TwitterController;
+use App\Http\Controllers\Api\CallbackController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -18,6 +19,6 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
-
+Route::post('callback', [CallbackController::class, 'index']);
 
 Route::get('twitter', [TwitterController::class, 'indexgetTweet']);
