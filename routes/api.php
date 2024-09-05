4c3ccf9d12e04141cf2d49fce8d3a4f1077f8392
Febryan Pratama <febryancaesarpratama@gmail.com>
2024-09-05 10:28:26 +0700

    feat: rev api

--- routes/api.php
+++ routes/api.php
@@ -19,6 +19,6 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
-Route::post('callback', [CallbackController::class, 'index']);
+Route::get('callback', [CallbackController::class, 'index']);
 
 Route::get('twitter', [TwitterController::class, 'indexgetTweet']);
