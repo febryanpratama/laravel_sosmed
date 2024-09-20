d965d3c5b88e8da00d72cad373dd061b69aca72f
Roni Surya <ronisurya015@gmail.com>
2024-09-20 13:11:54 +0700

    feat: Adding Middleware for  Menu

--- routes/web.php
+++ routes/web.php
@@ -3,13 +3,8 @@
 use App\Http\Controllers\AccountController;
 use App\Http\Controllers\Auth\SocialiteController;
 use App\Http\Controllers\AuthController;
-use App\Http\Controllers\CheckoutController;
-use App\Http\Controllers\DokterController;
 use App\Http\Controllers\EmailController;
-use App\Http\Controllers\FarmasiController;
 use App\Http\Controllers\KontenController;
-use App\Http\Controllers\PendaftaranController;
-use App\Http\Controllers\TiketController;
 use App\Http\Controllers\WaController;
 use Illuminate\Support\Facades\Route;
 
@@ -29,53 +24,54 @@ Route::get('/', function () {
 });
 
 Route::prefix('auth')->group(function () {
-    Route::get('login', [AuthController::class, 'index']);
+    Route::get('login', [AuthController::class, 'index'])->name('login');
     Route::post('login', [AuthController::class, 'login']);
     Route::get('register', [AuthController::class, 'register']);
     Route::post('register', [AuthController::class, 'store']);
 });
 
-Route::get('home', function () {
-    return view('pages.home.index');
-});
-
-Route::prefix('konten')->group(function () {
-    Route::get('/', [KontenController::class, 'index']);
-    Route::post('/', [KontenController::class, 'store'])->name('konten.store');
-});
-
-Route::prefix('account')->group(function () {
-    Route::get('/', [AccountController::class, 'index']);
-    Route::post('/', [AccountController::class, 'store']);
-});
-
-Route::prefix('broadcast')->group(function () {
-    // Email Controller
-    Route::prefix('email')->group(function () {
-        Route::get('/', [EmailController::class, 'index']);
-        Route::get('/create', [EmailController::class, 'create']);
-        Route::post('/create', [EmailController::class, 'store']);
-        Route::get('/send/{email}', [EmailController::class, 'sendEmail']);
-        Route::get('/edit/{email}', [EmailController::class, 'edit']);
-        Route::get('/hapus/{email}', [EmailController::class, 'hapus']);
-        Route::post('/update', [EmailController::class, 'update']);
+Route::middleware('checksession')->group(function () {
+    Route::get('home', function () {
+        return view('pages.home.index');
     });
 
-    // Whatsapp Controller
-
-    Route::prefix('whatsapp-blast')->group(function () {
-        Route::get('/', [WaController::class, 'index']);
-        Route::get('/create', [WaController::class, 'create']);
+    Route::prefix('konten')->group(function () {
+        Route::get('/', [KontenController::class, 'index']);
+        Route::post('/', [KontenController::class, 'store'])->name('konten.store');
     });
-});
+
+    Route::prefix('account')->group(function () {
+        Route::get('/', [AccountController::class, 'index']);
+        Route::post('/', [AccountController::class, 'store']);
+    });
+
+    Route::prefix('broadcast')->group(function () {
+        // Email Controller
+        Route::prefix('email')->group(function () {
+            Route::get('/', [EmailController::class, 'index']);
+            Route::get('/create', [EmailController::class, 'create']);
+            Route::post('/create', [EmailController::class, 'store']);
+            Route::get('/send/{email}', [EmailController::class, 'sendEmail']);
+            Route::get('/edit/{email}', [EmailController::class, 'edit']);
+            Route::get('/hapus/{email}', [EmailController::class, 'hapus']);
+            Route::post('/update', [EmailController::class, 'update']);
+        });
+
+        // Whatsapp Controller
+        Route::prefix('whatsapp-blast')->group(function () {
+            Route::get('/', [WaController::class, 'index']);
+            Route::get('/create', [WaController::class, 'create']);
+        });
+    });
 
 
-Route::get('auth/twitter', [SocialiteController::class, 'redirectToProviderTwitter']);
-Route::get('auth/instagram', [SocialiteController::class, 'redirectToProviderInstagram']);
+    Route::get('auth/twitter', [SocialiteController::class, 'redirectToProviderTwitter']);
+    Route::get('auth/instagram', [SocialiteController::class, 'redirectToProviderInstagram']);
 
-Route::middleware(['web'])->group(function () {
-    Route::get('auth/twitter/callback', [SocialiteController::class, 'handleProviderCallbackTwitter']);
-    Route::get('auth/instagram/callback', [SocialiteController::class, 'handleProviderCallbackInstagram']);
+    Route::middleware(['web'])->group(function () {
+        Route::get('auth/twitter/callback', [SocialiteController::class, 'handleProviderCallbackTwitter']);
+        Route::get('auth/instagram/callback', [SocialiteController::class, 'handleProviderCallbackInstagram']);
+    });
 });
 
 
